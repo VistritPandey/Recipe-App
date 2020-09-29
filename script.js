5b20efc091ccc576d0f10bcf8949034df665b870
vistritpandey <vistritpandey@gmail.com>
2020-09-29 18:21:35 +0530

    b

--- script.js
+++ script.js
@@ -36,3 +36,45 @@ async function getMealsBySearch(term) {
 
     return meals;
 }
+function addMeal(mealData, random = false) {
+    console.log(mealData);
+
+    const meal = document.createElement("div");
+    meal.classList.add("meal");
+
+    meal.innerHTML = `
+        <div class="meal-header">
+            ${
+                random
+                    ? `
+            <span class="random"> Random Recipe </span>`
+                    : ""
+            }
+            <img
+                src="${mealData.strMealThumb}"
+                alt="${mealData.strMeal}"
+            />
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn">
+                <i class="fas fa-heart"></i>
+            </button>
+        </div>
+    `;
+    const btn = meal.querySelector(".meal-body .fav-btn");
+    btn.addEventListener("click", () => {
+        if (btn.classList.contains("active")) {
+            removeMealLS(mealData.idMeal);
+            btn.classList.remove("active");
+        } else {
+            addMealLS(mealData.idMeal);
+            btn.classList.add("active");
+        }
+        fetchFavMeals();
+    });
+    meal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+    mealsEl.appendChild(meal);
+}
